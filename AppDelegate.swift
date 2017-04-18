9c390740c06b5f1c25bab3f748737c7b63c255a7
Kaleb Cooper <Kaleb@Kalebs-MacBook-Pro-2.local>
2017-04-17 23:34:00 -0500

    Initial Commit

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoClutter
+//
+//  Created by Kaleb Cooper on 4/17/17.
+//  Copyright © 2017 Kaleb Cooper. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoClutter
+//
+//  Created by Kaleb Cooper on 4/17/17.
+//  Copyright © 2017 Kaleb Cooper. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
